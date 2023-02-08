84defc1f8cc9c412879c99433b7a3be3fc54805e
Amirreza <amirreza.m13825@gmail.com>
2023-02-08 23:21:22 +0330

    state of the form and chandHandler function added

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,18 +1,46 @@
+import { useState } from "react";
+
 const SignUpForm = () => {
+  const [formValue, setFormValue] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  // Handler
+  const changeHandler = (e) => {
+    setFormValue({ ...formValue, [e.target.id]: e.target.value });
+  };
+
   return (
     <div>
       <form>
         <div className="formControl">
           <label htmlFor="name">Name</label>
-          <input type="text" id="name" />
+          <input
+            type="text"
+            id="name"
+            value={formValue.name}
+            onChange={changeHandler}
+          />
         </div>
         <div className="formControl">
           <label htmlFor="email">Email</label>
-          <input type="text" id="email" />
+          <input
+            type="text"
+            id="email"
+            value={formValue.email}
+            onChange={changeHandler}
+          />
         </div>
         <div className="formControl">
-          <label htmlFor="pass">Password</label>
-          <input type="text" id="pass" />
+          <label htmlFor="password">Password</label>
+          <input
+            type="text"
+            id="password"
+            value={formValue.password}
+            onChange={changeHandler}
+          />
         </div>
         <button type="submit">Submit</button>
       </form>
